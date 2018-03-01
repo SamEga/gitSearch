bd396d1623847ddbf7dfa86052c44313c60016f4
SamEga <egorstepanow@mail.ru>
2018-03-01 13:16:17 +0700

    Add files via upload

---
+++ github.js
@@ -0,0 +1,31 @@
+class Github {
+  constructor() {
+    this.client_id = "58602a1437203cd2822e";
+    this.client_secret = "2e3d3e9a9ad063230243ddc095fb21a9cf65c91b";
+    this.repos_count = 4;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per-page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
